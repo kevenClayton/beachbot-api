f461c7529f2fe08d638f519d562b3faf8661a270
Kevenclayton <kevenclaytonalves@hotmail.com>
2023-02-24 19:25:53 -0300

    retornar horarios quase ok

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UsuarioController;
+use App\Http\Controllers\Api\AgendamentoController;
 
 
 /*
@@ -22,6 +23,16 @@ Route::middleware(['auth:sanctum', 'treblle'])->group(function () {
     Route::controller(UsuarioController::class)->prefix('usuario')->group(function () {
         Route::get('/listar', 'listar');
         Route::get('/mostrar', 'mostrar');
+    });
+
+    Route::controller(AgendamentoController::class)->prefix('agendamento')->group(function () {
+        Route::get('/horarios-disponiveis-1-dia', 'carregarHorariosDisponveisPorDia');
+        Route::get('/retornar-proximos-7-dias', 'retornarProximos7Dias');
+        Route::get('/horario-funcionamento', 'horariosFuncionamentoEstabelecimento');
+        Route::get('/todos-horarios-disponiveis', 'todosHorariosDisponiveis');
+        Route::get('/todos-horarios-marcados', 'retornarTodosHorariosAgendados');
+        Route::get('/horarios-disponiveis-um-item/{tipo}', 'horariosDisponiveisUmItem'); // Pegar horário disponível somente de um item, exemplo quadra futvolei
+        Route::post('/marcar-horario-um-item', 'horariosDisponiveisUmItem');
     });
 
 });
