8bde2dff8b0db1516eaed83ee9861470fd6266f0
Kevenclayton <kevenclaytonalves@hotmail.com>
2023-03-02 21:31:45 -0300

    Alterando status code para 404

--- app/Http/Controllers/Api/AgendamentoController.php
+++ app/Http/Controllers/Api/AgendamentoController.php
@@ -21,130 +21,6 @@ use DateTime;
 
 class AgendamentoController extends Controller
 {
-    // public function horariosFuncionamentoEstabelecimento(){
-
-    //     $horariosFuncionamentoSegunda = HorarioFuncionamento::where('dia_semana_horario_funcionamento','segunda')->first(['horario_funcionamento']);
-    //     $horariosFuncionamentoTerca = HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'terca')->first(['horario_funcionamento']);
-    //     $horariosFuncionamentoQuarta = HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'quarta')->first(['horario_funcionamento']);
-    //     $horariosFuncionamentoQuinta = HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'quinta')->first(['horario_funcionamento']);
-    //     $horariosFuncionamentoSexta = HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'sexta')->first(['horario_funcionamento']);
-    //     $horariosFuncionamentoSabado = HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'sabado')->first(['horario_funcionamento']);
-    //     $horariosFuncionamentoDomingo = HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'domingo')->first(['horario_funcionamento']);
-
-    //     return response()->json([
-    //         'message' => 'Horários de funcionamento do estabelecimento retornado com sucesso!',
-    //         'horarios'=> [
-    //             'segunda'=>$horariosFuncionamentoSegunda->horario_funcionamento,
-    //             'terca'=>$horariosFuncionamentoTerca->horario_funcionamento,
-    //             'quarta'=>$horariosFuncionamentoQuarta->horario_funcionamento,
-    //             'quinta'=>$horariosFuncionamentoQuinta->horario_funcionamento,
-    //             'sexta'=>$horariosFuncionamentoSexta->horario_funcionamento,
-    //             'sabado'=>$horariosFuncionamentoSabado->horario_funcionamento,
-    //             'domingo'=>$horariosFuncionamentoDomingo->horario_funcionamento,
-    //         ],
-    //         ], 200);
-
-
-
-    //  }
-    // public function todosHorariosDisponiveis2(){
-
-    //     $horariosFuncionamentoSegunda    =      HorarioFuncionamento::where('dia_semana_horario_funcionamento','segunda')->first(['horario_funcionamento_inicio','horario_funcionamento_fim']);
-    //     $horariosFuncionamentoTerca      =      HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'terca')->first(['horario_funcionamento_inicio','horario_funcionamento_fim']);
-    //     $horariosFuncionamentoQuarta     =      HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'quarta')->first(['horario_funcionamento_inicio','horario_funcionamento_fim']);
-    //     $horariosFuncionamentoQuinta     =      HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'quinta')->first(['horario_funcionamento_inicio','horario_funcionamento_fim']);
-    //     $horariosFuncionamentoSexta      =      HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'sexta')->first(['horario_funcionamento_inicio','horario_funcionamento_fim']);
-    //     $horariosFuncionamentoSabado     =      HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'sabado')->first(['horario_funcionamento_inicio','horario_funcionamento_fim']);
-    //     $horariosFuncionamentoDomingo    =      HorarioFuncionamento::where('dia_semana_horario_funcionamento', 'domingo')->first(['horario_funcionamento_inicio','horario_funcionamento_fim']);
-
-    //     return Carbon::parse('2023-02-27 19:00:00')->locale('pt-br')->dayName;
-
-    //     $horariosReservados = HorariosReservadosEspacos::all('data_hora_inicio_reservado_espaco', 'data_hora_fim_reservado_espaco');
-    //     return response()->json([
-    //         'message' => 'Horários de funcionamento do estabelecimento retornado com sucesso!',
-    //         'horariosReservados'=> $horariosReservados,
-    //         ], 200);
-    //     dd($horariosReservados);
-
-
-    //     $inicio_pretendido = '2023-02-23 19:00:00';
-    //     $final_pretendido = '2023-02-23 20:00:00';
-
-    //     $inicio_existente = '2023-02-23 14:00:00';
-    //     $final_existente = '2023-02-23 23:00:00';
-
-    //     $livre = $inicio_pretendido >= $final_existente || $final_pretendido <= $inicio_existente;
-    //     dd($livre);
-
-
-    // }
-    // public function horariosDisponiveisUmItem($tipo){
-
-    // }
-
-    // public function todosHorariosDisponiveisk(){
-    //     $horario_de_funcionamento = ['inicio' => '09:00', 'fim' => '18:00'];
-    //     $horarios_agendados = ['10:00', '12:00', '15:00', '16:30'];
-
-    //     $horarios_disponiveis = [];
-
-    //     $horario_atual = $horario_de_funcionamento['inicio'];
-
-    //     while ($horario_atual < $horario_de_funcionamento['fim']) {
-    //         if (!in_array($horario_atual, $horarios_agendados)) {
-    //             $horarios_disponiveis[] = $horario_atual;
-    //         }
-
-    //         $horario_atual = adicionar_horario($horario_atual, 30); // adicionar 30 minutos
-    //     }
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // private function retornarTodosHorariosAgendados()
-    // {
-    //     $horariosReservados = HorariosReservadosEspacos::all('data_hora_inicio_reservado_espaco')
-    //                                 ->pluck('hora_inicio_reservado_espaco')
-    //                                 ->toArray();
-    //     return $horariosReservados;
-    // }
-
-
-    // public function todosHorariosDisponiveis($periodo = 'dia')
-    // {
-    //     $horario_de_funcionamento = ['inicio' => '14:00', 'fim' => '23:00'];
-
-    //     $horarios_agendados = $this->retornarTodosHorariosAgendados(); // função para obter horários já agendados
-    //     $horariosDisponiveis = $this->calcular_horarios_disponiveis($horario_de_funcionamento, $horarios_agendados);
-
-    //     return response()->json([
-    //         'message' => 'Horários de funcionamento do estabelecimento retornado com sucesso!',
-    //         'horariosDisponiveis'=> $horariosDisponiveis,
-    //         ], 200);
-    // }
-
-
-    // Retornar horários livres do dia
-
-    // Retornar horários ocupados do mês
-
-
-    // ---- CHAMADAS OK ------//
 
     public function criarReservaHorario(Request $request){
         try{
@@ -209,7 +85,7 @@ class AgendamentoController extends Controller
             return response()->json([
                 'error'=> true,
                 'message'=> $e->getMessage(),
-            ], 400);
+            ], 404);
         }
     }
 
@@ -251,7 +127,7 @@ class AgendamentoController extends Controller
             return response()->json([
                 'error'=> true,
                 'message'=> $e->getMessage(),
-            ], 400);
+            ], 404);
         }
     }
 
@@ -313,7 +189,7 @@ class AgendamentoController extends Controller
             return response()->json([
                 'error'=> true,
                 'message'=> $e->getMessage(),
-            ], 400);
+            ], 404);
         }
     }
 
@@ -363,7 +239,7 @@ class AgendamentoController extends Controller
             return response()->json([
                 'error'=> true,
                 'message'=> $e->getMessage(),
-            ], 400);
+            ], 404);
         }
     }
     public function retornarHorariosDisponveisPorPeriodo(Request $request){
@@ -429,7 +305,7 @@ class AgendamentoController extends Controller
             return response()->json([
                 'error'=> true,
                 'message'=> $e->getMessage(),
-            ], 400);
+            ], 404);
         }
     }
     public function retornarHorariosDisponveisPorDia(Request $request){
@@ -479,7 +355,7 @@ class AgendamentoController extends Controller
             return response()->json([
                 'error'=> true,
                 'message'=> $e->getMessage(),
-            ], 400);
+            ], 404);
         }
     }
 
@@ -531,7 +407,7 @@ class AgendamentoController extends Controller
             return response()->json([
                 'error'=> true,
                 'message'=> $e->getMessage(),
-            ], 400);
+            ], 404);
         }
     }
 
@@ -637,7 +513,7 @@ class AgendamentoController extends Controller
             return response()->json([
                 'error'=> true,
                 'message'=> $e->getMessage(),
-            ], 400);
+            ], 404);
         }
 
     }
@@ -654,7 +530,7 @@ class AgendamentoController extends Controller
 
         return response()->json([
             'data' => $horariosDisponiveis,
-        ]);
+        ],200);
     }
 
     function adicionar_horario($horario, $minutos) {
